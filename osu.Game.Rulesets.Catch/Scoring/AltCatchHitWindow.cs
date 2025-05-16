aeb9105278467b6e6c0020a15c2fb7f8b5793a4e
orwenn22 <46846090+orwenn22@users.noreply.github.com>
2025-05-16 22:28:01 +0200

    add catch hit window how did i forget this omg

---
+++ osu.Game.Rulesets.Catch/Scoring/AltCatchHitWindow.cs
@@ -0,0 +1,33 @@
+// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using osu.Game.Rulesets.Scoring;
+
+namespace osu.Game.Rulesets.Catch.Scoring
+{
+    // Copied from TaikoHitWindow
+    public class AltCatchHitWindow : HitWindows
+    {
+        internal static readonly DifficultyRange[] TAIKO_RANGES =
+        {
+            new DifficultyRange(HitResult.Great, 50, 35, 20),
+            new DifficultyRange(HitResult.Ok, 120, 80, 50),
+            new DifficultyRange(HitResult.Miss, 135, 95, 70),
+        };
+
+        public override bool IsHitResultAllowed(HitResult result)
+        {
+            switch (result)
+            {
+                case HitResult.Great:
+                case HitResult.Ok:
+                case HitResult.Miss:
+                    return true;
+            }
+
+            return false;
+        }
+
+        protected override DifficultyRange[] GetRanges() => TAIKO_RANGES;
+    }
+}
